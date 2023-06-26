2b2a967f0d0e39efa3b4e80c967828cbf7176ab0
soumyaranjanmahunta1 <happysoumya3@gmail.com>
2023-06-26 18:40:06 +0530

    AfterProductDetailsPage complited

--- BackEnd/Controller/ProductController.js
+++ BackEnd/Controller/ProductController.js
@@ -9,3 +9,12 @@ export const getproducts = async(req, res) => {
     }
     
 }
+export const getproductbyId= async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const product = await Product.findOne({"id": id});
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json({message:err.message});
+    }
+}
--- BackEnd/Routes/route.js
+++ BackEnd/Routes/route.js
@@ -1,10 +1,11 @@
 import express from "express";
 // import usersignUp from "../Controller/userController";
-import { getproducts } from "../Controller/ProductController.js";
+import { getproducts,getproductbyId } from "../Controller/ProductController.js";
 import { usersignUp, userLogin } from "../Controller/userController.js";
 const router = express.Router();
 
 router.post("/signup", usersignUp);
 router.post("/login", userLogin);
 router.get("/products", getproducts);
+router.get("/product/:id", getproductbyId);
 export default router;
